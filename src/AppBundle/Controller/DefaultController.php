63e25610b18eb338703d724f5613588030151c86
PatrykMitula <patryk.mitula@gmail.com>
2017-03-06 22:52:43 +0100

    pagination,bootstrap,commentType form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+use AppBundle\Form\CommentType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,8 +14,35 @@ class DefaultController extends Controller {
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request) {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', []);
+        
+        
+
+        $qb = $this->getDoctrine()
+                ->getManager()
+                ->createQueryBuilder()
+                ->from('AppBundle:Post', 'p')
+                ->select('p');
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+                $qb, $request->query->get('page', 1), 2
+        );
+
+        return $this->render('default/index.html.twig', ['posts' => $pagination]);
+    }
+
+    /**
+     * @Route("/news/{id}", name="news_show")
+     */
+    public function showAction(Post $post) {
+        
+
+        $form = $this->createForm(CommentType::class);
+
+        return $this->render('default/news.html.twig', [
+                    'post' => $post,
+                    'form' => $form->createView()
+        ]);
     }
 
 }
--- src/AppBundle/Entity/Post.php
+++ src/AppBundle/Entity/Post.php
@@ -56,6 +56,10 @@ class Post {
      */
     private $comments;
 
+    public function __toString() {
+        return $this->title;
+    }
+
     /**
      * Get id
      *
